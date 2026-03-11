6253618ac81735bfce2f954b4e56b80fd3c3e5e5
bsrikanth38-ai <bsrikanth38@gmail.com>
2026-03-11 15:24:03 +0530

    coded UC3

---
+++ UC3.java
@@ -0,0 +1,15 @@
+public class UC3 { 
+       public static void main(String[] arg){
+            System.out.println(String.join(" ", "   ***   ", "   ***   ", "******   ", "  *****  "));
+			System.out.println(String.join(" ", " **   ** ", " **   ** ", "**    ** ", " **   ** "));
+			System.out.println(String.join(" ", "**     **", "**     **", "**     **", "**       "));
+			System.out.println(String.join(" ", "**     **", "**     **", "**    ** ", " **      "));
+			System.out.println(String.join(" ", "**     **", "**     **", "******   ", "  *****  "));
+			System.out.println(String.join(" ", "**     **", "**     **", "**       ", "      ** "));
+			System.out.println(String.join(" ", "**     **", "**     **", "**       ", "       **"));
+			System.out.println(String.join(" ", " **   ** ", " **   ** ", "**       ", " **   ** "));
+			System.out.println(String.join(" ", "   ***   ", "   ***   ", "**       ", "  *****  "));
+
+       }
+}     
+
